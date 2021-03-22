46ba861bb4e52a89836b981173a0d9f4c5cb8739
ANGELINA <ang@MacBook-Air.home>
2021-03-22 16:43:23 -0400

    completed/working on

---
+++ cake.py
@@ -0,0 +1,8 @@
+cakeorno = input("Do you want cake? (yes/no)")
+
+if cakeorno == "yes" or cakeorno == "YES" or cakeorno == "y":
+    print("Have some cake!");
+elif cakeorno == "no" or cakeorno == "NO" or cakeorno == "n":
+    print("No cake for you!");
+else:
+    print("Sorry, I don't understand."); # error message
---
+++ spanish.py
@@ -0,0 +1,14 @@
+
+      var translation = prompt("Give me a word in English...");
+      if (translation === "cat" || translation === "Cat" || translation === "CAT") {
+        console.log("gato");
+      }
+      else if (translation === "dog" || translation === "Dog" || translation === "DOG") {
+        console.log("dog");
+      }
+        else if (translation === "horse" || translation === "Horse" || translation === "HORSE") {
+        console.log("horse");
+        }
+      else {
+        console.log("No entiendo.");
+      }
